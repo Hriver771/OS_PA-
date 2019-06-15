b10063241a0e9aae4da3e297abc9605289ea78ce
haminji <21500771@handong.edu>
2019-06-15 20:00:41 +0900

    1.2

--- smalloc-1.2/smalloc.c
+++ smalloc-1.2/smalloc.c
@@ -6,14 +6,91 @@ sm_container_ptr sm_first = 0x0 ;
 sm_container_ptr sm_last = 0x0 ;
 sm_container_ptr sm_unused_containers = 0x0 ;
 
+void sm_ist_unused_list(sm_container_ptr hole)
+{
+	printf("########insert: %p\n", hole->data) ;
+	sm_container_ptr tmp = sm_unused_containers ;
+	if (tmp == NULL)
+	{
+		hole->next_unused = NULL ;
+		sm_unused_containers = hole ;
+		print_unused_list() ;
+		return ;
+	}
+	if (tmp == hole)
+		return ;
+	while (tmp->next_unused != NULL)
+	{
+		if(tmp->next_unused == hole)
+			return ;
+		tmp = tmp->next_unused ;		
+	}
+	tmp->next_unused = hole;
+	hole->next_unused = NULL ;
+	print_unused_list() ;
+}
+
+void sm_del_unused_list(sm_container_ptr rm)
+{
+	printf("@@@@@@@@@@@@ del:%p @@@@@@@@@@@\n", rm->data);
+	sm_container_ptr tmp = sm_unused_containers ;
+
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "ERROR: del_unused_list : EMPTY\n");
+		return ;
+	}
+	if (tmp == rm)
+	{
+		sm_unused_containers = rm->next_unused ;
+		rm->next_unused = NULL ;
+		print_unused_list() ;
+		return ;
+	}
+	while (tmp->next_unused != NULL)
+	{
+		if(tmp->next_unused == rm)
+			break ;
+		tmp = tmp->next_unused ;
+	}
+	if (tmp->next_unused != rm)
+	{
+		fprintf(stderr, "ERROR: del_unused_list : No such a link\n") ;
+		return ;
+	}
+	tmp->next_unused = rm->next_unused ;
+	rm->next_unused = NULL ;
+	print_unused_list() ;
+}
+
+void print_unused_list()
+{
+	sm_container_ptr tmp = sm_unused_containers ;
+	if(tmp == NULL)
+	{
+		printf("There is no unused container\n") ;
+		return ;
+	}
+	printf("%p", tmp->data) ;
+	while (tmp->next_unused != NULL)
+	{
+		tmp = tmp->next_unused ;
+		printf(" -> %p", tmp->data) ;
+	}
+	printf("\n");
+	return ;
+}
+
 void sm_container_split(sm_container_ptr hole, size_t size)
 {
+	sm_ist_unused_list(hole);
 	sm_container_ptr remainder = hole->data + size ;
 
 	remainder->data = ((void *)remainder) + sizeof(sm_container_t) ;
 	remainder->dsize = hole->dsize - size - sizeof(sm_container_t) ;
 	remainder->status = Unused ;
 	remainder->next = hole->next ;
+	sm_ist_unused_list(remainder) ;
 	hole->next = remainder ;
 
 	if (hole == sm_last)
@@ -50,6 +127,7 @@ void * smalloc(size_t size)
 		if (size == itr->dsize) {
 			// a hole of the exact size
 			itr->status = Busy ;
+			sm_del_unused_list(itr) ;
 			return itr->data ;
 		}
 		else if (size + sizeof(sm_container_t) < itr->dsize) {
@@ -78,6 +156,7 @@ void * smalloc(size_t size)
 	sm_container_split(hole, size) ;
 	hole->dsize = size ;
 	hole->status = Busy ;
+	sm_del_unused_list(hole) ;
 	return hole->data ;
 }
 
@@ -89,6 +168,7 @@ void sfree(void * p)
 	for (itr = sm_first ; itr->next != 0x0 ; itr = itr->next) {
 		if (itr->data == p) {
 			itr->status = Unused ;
+			sm_ist_unused_list(itr) ;
 			break ;
 		}
 	}
--- smalloc-1.2/smalloc.h
+++ smalloc-1.2/smalloc.h
@@ -5,13 +5,16 @@ typedef enum {
 typedef struct _sm_container_t {
 	sm_container_status status ;
 	struct _sm_container_t * next ;
-	/* struct _sm_container_t * next_unused ; */
+	struct _sm_container_t * next_unused ;
 	size_t dsize ;
 	void * data ;
 } sm_container_t ;
 
 typedef sm_container_t * sm_container_ptr ;
 
+void print_unused_list();
+void sm_del_unused_list(sm_container_ptr) ;
+void sm_ist_unused_list(sm_container_ptr) ;
 void * smalloc(size_t size) ; 
 void sfree(void * p) ;
 void print_sm_containers() ;
